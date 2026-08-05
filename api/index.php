fa1434f033cdc985288c318314df0c790b6b8e11
GusWhyu <jadennx17@gmail.com>
2026-08-05 21:36:34 +0800

    Fix Vercel read-only filesystem crash by moving cache paths to /tmp

--- api/index.php
+++ api/index.php
@@ -1,9 +1,2 @@
 <?php
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    echo '<pre>';
-    echo 'Raw Exception: ' . $e->getMessage() . "\n";
-    echo $e->getTraceAsString();
-    echo '</pre>';
-}
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,10 +14,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (\Throwable $e) {
-            echo '<pre>PRIMARY EXCEPTION: ' . $e->getMessage() . "\n";
-            echo $e->getTraceAsString();
-            echo '</pre>';
-            die();
-        });
+        //
     })->create();
